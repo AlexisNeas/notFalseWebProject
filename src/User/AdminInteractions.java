56d2ae22f4df35e3c1e6c5e2e79fe6957d268b87
ANEAS001 <aneas001@vplincsci057.ad.csbsju.edu>
2018-04-18 14:51:49 -0500

    Finished manage users

--- src/User/AdminInteractions.java
+++ src/User/AdminInteractions.java
@@ -94,9 +94,15 @@ public class AdminInteractions {
    * @param type
    */
   public int addNewUser(String firstName, String lastName, String username, String password, char type){
-    User user = new User(firstName, lastName, username, password, type, 'Y');
-    int added = adminController.addNewUser(user);
-    return added;
+    try {
+    	Account user = new Account(firstName, lastName, username, password, type, 'Y');
+    	int added = adminController.addNewUser(user);
+        return added;  
+    }
+    catch(IllegalArgumentException e)
+    {
+    		return -2;
+    }
   }
   
   /**
